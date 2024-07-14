3b8f0b174c0b5dce08f7c4aaaca6d8471c611ec0
dhsig86 <dr.dhsig@gmail.com>
2024-07-14 10:28:12 -0300

    Remover ambiente virtual duplicado e corrigir configuração

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,13 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
-
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
 
-# Importar rotas após inicialização do app e db
 from routes import *
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-
-app = Flask(__name__)
-CORS(app)
-
-app.config.from_object('config.Config')
-db = SQLAlchemy(app)
-
-# Importar rotas após inicialização do app e db
-from routes import *
-
-if __name__ == '__main__':
-    app.run()
-
 
 if __name__ == '__main__':
     app.run()
