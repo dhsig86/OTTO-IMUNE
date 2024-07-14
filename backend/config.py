27af75422d6bd1b4c7ad94529d38685049c48a5c
dhsig86 <dr.dhsig@gmail.com>
2024-07-14 13:37:56 -0300

    Ajustando a configuração do banco de dados

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace('postgres://', 'postgresql+psycopg2://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
