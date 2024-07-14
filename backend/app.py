93bae93b404b3f1a9925c0ee4b51499fa42fc8f8
dhsig86 <dr.dhsig@gmail.com>
2024-07-14 03:49:25 -0300

    Configurar Flask-CORS e atualizar configurações

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,13 @@
-from routes import app
+from flask import Flask
+from flask_cors import CORS
+from models import db, init_app
+from routes import main
 
-if __name__ == "__main__":
-    app.run()
+app = Flask(__name__)
+init_app(app)
+CORS(app)
+
+app.register_blueprint(main)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
